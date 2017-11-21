a4bad8aa14664f75b001213c46c2121635eda019
Plax96 <placido.castellanos@gmail.com>
2017-11-21 11:38:55 -0600

    segundo

--- enunciado/admin.py
+++ enunciado/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
+
+admin.site.register(Alumno)
+admin.site.register(Profesor)
+admin.site.register(Materia)
+admin.site.register(Nota)
 # Register your models here.
--- enunciado/models.py
+++ enunciado/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+class Grado(models.Model):
+    nombre = models.CharField(max_length=50)
+    seccion= models.CharField(max_length=2)
+    def __str__(self):
+        return self.nombre
+
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido =models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido =models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=30,blank=True, null=True)
+    estado = models.CharField(max_length=2,default="1")
+    alumno = models.ForeignKey(Alumno, on_delete = models.CASCADE, blank=True, null=True)
+    profesor = models.ForeignKey(Profesor, on_delete = models.CASCADE, blank=True, null=True)
+    def __str__(self):
+        return self.nombre
+
+class Nota(models.Model):
+    nombre = models.CharField(max_length=30,blank=True, null=True)
+    punteo = models.IntegerField(null=True)
+    materia = models.ForeignKey(Materia, on_delete = models.CASCADE,blank=True, null=True)
+    alumno = models.ForeignKey(Alumno, on_delete = models.CASCADE,blank=True, null=True)
+    def __str__(self):
+        return self.nombre
+
+
+
 # Create your models here.
